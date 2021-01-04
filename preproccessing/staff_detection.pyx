c4da7cf46f5d675bc74469a5b43597388894694f
bahaaEldeen1999 <bahaaeldeen1999@gmail.com>
2021-01-04 23:47:09 +0200

    charachter

--- preproccessing/staff_detection.pyx
+++ preproccessing/staff_detection.pyx
@@ -324,29 +324,35 @@ def lineSegmentationBasedOnStaves(img, staff_lines):
     return newImgs
 
 
-def charachterSegmentation(img, t):
+def charachterSegmentation(img_o, t):
     # img = sk.transform.resize(
     #     img, output_shape=(img.shape[0]*2, img.shape[1]*4), mode="constant", cval=0)
     # img = img > 0
     # print(img.shape)
     # show_images([img])
-    #se = np.ones((1, 4))
-    # img_n = sk.morphology.erosion(img, se)
+    # se = np.ones((1, 4))
+    img = np.copy(img_o)
+    # img = sk.morphology.binary_closing(img)
     # img_n = img_n ^ img
     # show_images([img_n])
     vertical_hist = np.sum(img, axis=0, keepdims=True)
     # print(vertical_hist)
     chars = []
+    chars1 = []
     i = 0
     while i < img.shape[1]:
         if vertical_hist[0][i] > t:
             j = i + 1
-            while j < img.shape[1] and vertical_hist[0][j] > t or abs(i-j) < 10:
+            while j < img.shape[1] and vertical_hist[0][j] > t:
                 j += 1
             # print("i "+str(i)+" j "+str(j))
-            chars.append(img[:, i:j])
+            # chars1.append(img[:, i:j])
+            c1 = max(0, i-2)
+            c2 = min(img.shape[1], j+2)
+            chars.append([0, img.shape[0], c1, c2])
             i = j - 1
         i += 1
+    # show_images(chars1)
     return chars
 
 
@@ -374,36 +380,47 @@ def removeMusicalNotes(img, T_LEN):
 
 
 def extractCircleNotes(img, staff_height):
+    # print(staff_height)
     newImg = np.copy(img)
-    se = np.ones((staff_height+1, staff_height+1))
-    newImg = sk.morphology.binary_erosion(newImg, se)
+    # se = np.ones(((staff_height)*2, (staff_height)*2))
+    # newImg = sk.morphology.binary_dilation(newImg, se)
+    se = np.ones(((staff_height+1)*2, (staff_height+1)*2))
+    newImg = sk.morphology.binary_opening(newImg, se)
+    # newImg = sk.morphology.binary_erosion(newImg, se)
     return newImg
 
 
-def classicLineSegmentation(img_bin, staff_space=0):
-    org = np.copy(img_bin)
+def classicLineSegmentation(img, staff_space=0):
+    org = np.copy(img)
     lines = []
     se = np.ones((staff_space+5, 2))
-    img_bin = sk.morphology.binary_dilation(img_bin, se)
-    se = np.ones((staff_space*4, 2))
-    img_bin2 = sk.morphology.binary_erosion(img_bin, se)
-
-    img = img_bin2.astype(np.uint8)
-    img *= 255
-    #show_images([img_bin, img_bin2])
-    contours, hierarchy = cv.findContours(
-        img, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-    for cnt in contours:
-        x, y, w, h = cv.boundingRect(cnt)
-        if w/h >= 3:
-            lines.append([y, y+h, x, x+w])
-            show_images([img[y: y+h, x: x+w]])
+    img = sk.morphology.binary_dilation(img, se)
+    horz_hist = np.sum(img, axis=1)
+    t = 0.25
+    i = 0
+    j = 0
+    while i < img.shape[0]:
+        if horz_hist[i]/img.shape[1] >= t:
+            j = i + 1
+            while j < img.shape[0] and horz_hist[j]/img.shape[1] >= t:
+                j += 1
+            # print("i "+str(i)+" j "+str(j))
+            r0 = max(0, i-staff_space+5)
+            r1 = min(img.shape[0], j+staff_space+5)
+            lines.append([r0, r1, 0, img.shape[1]])
+            i = j - 1
+        i += 1
+    # show_images(lines)
     return lines
 
 
 def runCode():
     cdef int T_LEN
-    folder = 'test_new'
+    folder = 'easy'
+    try:
+        os.mkdir(folder + "Out")
+    except:
+        pass
     for filename in os.listdir(folder):
         print("file "+str(filename))
         img = sk.io.imread(os.path.join(folder, filename), as_gray=True)
@@ -415,18 +432,21 @@ def runCode():
         img = img.astype(np.float64) / np.max(img)
         img = 255 * img
         img = img.astype(np.uint8)
-        img = binarize(img)
-        img = sk.morphology.binary_closing(img)
+        img = binarize(img, 101)
+        # img_o = np.copy(img)
+        # show_images([img_o])
+        img = sk.morphology.binary_dilation(img)
         staff_height, staff_space = verticalRunLength(img)
         T_LEN = min(2*staff_height, staff_height+staff_space)
-
+        ########
+        img_1 = extractCircleNotes(img, staff_height)
+        img_1 = img_1 > 0
+        # show_images([img_1])
+        ##########
         #####
         lines = classicLineSegmentation(img, staff_space)
         ######
         print(img.shape)
-        img_1 = extractCircleNotes(img, staff_height)
-        img_1 = img_1 > 0
-        # show_images([img_1])
         # removed_staff, staff_lines = staffLineDetection(img, staff_space, staff_height)
         # show_images([removed_staff])
         removed_staff = removeMusicalNotes(img, T_LEN)
@@ -435,13 +455,36 @@ def runCode():
         # show_images([removed_staff])
         # io.imsave("outputE/"+filename+"_removed_stadd.png",
         # sk.img_as_uint(removed_staff))
-        se = np.ones((staff_height+1, staff_height+1))
-        removed_staff = sk.morphology.binary_closing(removed_staff, se)
-        removed_staff = sk.morphology.binary_dilation(removed_staff)
+        se = np.ones((staff_height*2+1, staff_height*2+1))
+        removed_staff_d = np.copy(removed_staff)
+        removed_staff_d = sk.morphology.binary_dilation(removed_staff_d, se)
+        # se = np.ones((1, staff_height+1))
+        # removed_staff = sk.morphology.binary_dilation(removed_staff_d, se)
+        # show_images([removed_staff])
+        # removed_staff = sk.morphology.binary_dilation(removed_staff)
+        i = 0
+        try:
+            os.mkdir(folder + "Out/"+filename+"_img")
+        except:
+            pass
         for line in lines:
             try:
-                show_images(charachterSegmentation(
-                    removed_staff[line[0]:line[1], line[2]:line[3]], 0))
+                i += 1
+                chars = charachterSegmentation(
+                    removed_staff[line[0]:line[1], line[2]:line[3]], 0*staff_height*2)
+                try:
+                    os.mkdir(folder + "Out/"+filename +
+                             "_img/"+filename+"_lines"+str(i))
+                except:
+                    pass
+                j = 0
+                for char in chars:
+                    # print(char)
+                    # show_images(
+                    #     [removed_staff[line[0]:line[1], line[2]:line[3]][char[0]:char[1], char[2]:char[3]]])
+                    io.imsave(folder + "Out/"+filename+"_img/"+filename+"_lines"+str(i)+"/"+filename+"_line_"+str(i) + "_char_"+str(
+                        j)+".png", sk.img_as_uint(removed_staff[line[0]:line[1], line[2]:line[3]][char[0]:char[1], char[2]:char[3]]))
+                    j += 1
             except:
                 continue
         # io.imsave(folder+"Out/"+filename+"_removed_stadd_closing.png",sk.img_as_uint(removed_staff))
