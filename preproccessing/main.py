8ab7a50c1d4243f38f4e8fc5c038ca534e3f252d
bahaaEldeen1999 <bahaaeldeen1999@gmail.com>
2021-01-11 16:44:46 +0200

    add new segmentation method and add splitting the image to extract lines locally

--- preproccessing/main.py
+++ preproccessing/main.py
@@ -6,7 +6,7 @@ import os
 from operator import itemgetter
 
 
-def mainPipeLine(img_original):
+def mainPipeLine(filename, img_original):
     # load model
     if os.path.isfile('finalized_model.sav'):
         ##### the model already exit####
@@ -28,49 +28,96 @@ def mainPipeLine(img_original):
     # get copy of priginal image
     img_o = np.copy(img)
     # convert from True False to 0-1
-    img = np.array(img)
+    #img = np.array(img)
     # Get Staff-height, Staffs-Space
-    staff_height, staff_space = verticalRunLength(img)
-    # get threshold based on staff height and space to remove lines
-    T_LEN = staff_height
-    # extract filled circle to add it to removed lines
-    img_filled_notes = extractCircleNotes(img, staff_height)
-    img_filled_notes = img_filled_notes > 0
-    # remove lines from image
-    removed_staff = extractMusicalNotes(img, T_LEN)
-    removed_staff = removed_staff > 0
-    removed_staff = removed_staff | img_filled_notes
 
+    #################################
+    # work on multiple images       #
+    #################################
+    patch_height = img.shape[0]//2
+    patch_width = img.shape[1]//10
+    ph = int(img.shape[0]/patch_height)
+    pw = int(img.shape[1]/patch_width)
+    removed_staff_c = np.copy(img)
+    staff_height_g, staff_space_g = verticalRunLength(img)
+    for i in range(ph):
+        for j in range(pw):
+            try:
+                # print("here")
+                patch = img[i*patch_height: (i+1)*patch_height,
+                            j*patch_width: (j+1)*patch_width]
+                staff_height, staff_space = verticalRunLength(patch)
+                # get threshold based on staff height and space to remove lines
+                T_LEN = staff_height
+                # extract filled circle to add it to removed lines
+                img_filled_notes = extractCircleNotes(patch, staff_height)
+                img_filled_notes = img_filled_notes > 0
+                # remove lines from image
+                removed_staff = extractMusicalNotes(patch, T_LEN)
+                removed_staff = removed_staff > 0
+                removed_staff = removed_staff | img_filled_notes
+                # io.imsave("test_newOut/patch_"+str(i)+"_"+str(j)+".png", sk.img_as_uint(
+                #     removed_staff))
+                removed_staff_c[i*patch_height: (i+1)*patch_height,
+                                j*patch_width: (j+1)*patch_width] = removed_staff
+            except:
+                pass
+    # show_images([removed_staff_c])
+    #removed_staff_c = sk.morphology.binary_opening(removed_staff_c)
+    io.imsave("test_newOut/removed_staff"+filename+".png", sk.img_as_uint(
+        removed_staff_c))
+    print("done "+filename)
+    # return
+    #########################################
     # extract staff lines from image
-    T_LEN = min(2*staff_height, staff_height+staff_space)
+    T_LEN = min(2*staff_height_g, staff_height_g+staff_space_g)
     staffLines = removeMusicalNotes(img, T_LEN)
     # get fixed staff lines
     se = np.ones((1, 55))
     fixed_staff_lines = restoreStaffLines(staffLines, T_LEN, img_o)
     fixed_staff_lines = sk.morphology.binary_opening(fixed_staff_lines, se)
     fixed_staff_lines = fixStaffLines(
-        fixed_staff_lines, staff_height, staff_space, img_o)
+        fixed_staff_lines, staff_height_g, staff_space_g, img_o)
     fixed_staff_lines = sk.morphology.binary_opening(fixed_staff_lines, se)
     # get staff lines from image
-    lines = classicLineSegmentation(img, staff_space)
+    lines = classicLineSegmentation(img, staff_space_g)
     i = 0
+    try:
+        os.mkdir("test_newOut/"+filename+"_img")
+    except:
+        pass
     for line in lines:
         lineOut = []
         # get notes bounds from the image without the staff lines
-        #show_images([removed_staff[line[0]:line[1], line[2]:line[3]]])
-        bounds = char_seg(removed_staff[line[0]:line[1], line[2]:line[3]])
+        #show_images([removed_staff_c[line[0]:line[1], line[2]:line[3]]])
+        bounds, only_char_arr = char_seg(
+            removed_staff_c[line[0]:line[1], line[2]:line[3]])
+        i += 1
+        try:
+            os.mkdir("test_newOut/"+filename +
+                     "_img/"+filename+"_lines"+str(i))
+        except Exception as e:
+            pass
+        # print("here")
+        j = 0
         for char in bounds:
-            i += 1
+            if char[0] == 99999999:
+                continue
+            charImg = only_char_arr[j]
+            j += 1
+            #i += 1
             try:
                 #######
                 # classify note
-                char = [0, removed_staff[line[0]:line[1], line[2]
-                    :line[3]].shape[0], int(char[2])-2, int(char[3])+2]
-                if char[3]-char[2] < staff_space:
-                    continue
-                charImg = removed_staff[line[0]: line[1], line[2]                                        : line[3]][char[0]: char[1], char[2]: char[3]]
-                # io.imsave("test_newOut\char_"+str(i)+".png", sk.img_as_uint(
-                #     charImg))
+                char = [0, removed_staff_c[line[0]:line[1], line[2]:line[3]].shape[0], int(char[2])-2, int(char[3])+2]
+                # print(char)
+                # if char[3]-char[2] < staff_space_g:
+                #     continue
+                # charImg = removed_staff_c[line[0]: line[1], line[2]                                          : line[3]][char[0]: char[1], char[2]: char[3]]
+                # show_images([charImg])
+                io.imsave("test_newOut/"+filename+"_img/"+filename+"_lines"+str(i)+"/"+filename+"_line_"+str(i) + "_char_"+str(
+                    j)+".png", sk.img_as_uint(
+                    charImg))
                 # charImg = np.array(charImg)
                 # a,b,c,d ==> wrong classificion
                 # start - # - ##  - & - && - end - timestamp - empty string True => end
@@ -78,8 +125,8 @@ def mainPipeLine(img_original):
 
                 # symbol = loaded_model.predict([extract_features(charImg)])
                 # print(symbol)
-                print(check_chord_or_beam(charImg, staff_space))
-                show_images([charImg])
+                #print(check_chord_or_beam(charImg, staff_space))
+                # show_images([charImg])
                 # getFlatHeadNotePos(fixed_staff_lines[line[0]: line[1], line[2]: line[3]], removed_staff[line[0]: line[1],
                 #                                                                                         line[2]: line[3]][char[0]: char[1], char[2]: char[3]], staff_space, char, staff_height)
             except Exception as e:
@@ -99,21 +146,21 @@ def mainPipeLine(img_original):
             # except Exception as e:
             #     print(e)
             #     pass
-        print("--------Line----------")
-        lineOut = sorted(lineOut, key=itemgetter(0))
-        lineS = ""
-        for arr in lineOut:
-            if arr[0] == -1:
-                continue
-            a = arr[1:]
-            a = np.sort(a)
-            if not len(a):
-                continue
-            lineS += "{ "
-            for x in a:
-                lineS += x+" "
-            lineS += "}"
-        print(lineS)
+        # print("--------Line----------")
+        # lineOut = sorted(lineOut, key=itemgetter(0))
+        # lineS = ""
+        # for arr in lineOut:
+        #     if arr[0] == -1:
+        #         continue
+        #     a = arr[1:]
+        #     a = np.sort(a)
+        #     if not len(a):
+        #         continue
+        #     lineS += "{ "
+        #     for x in a:
+        #         lineS += x+" "
+        #     lineS += "}"
+        # print(lineS)
     return img
 
 
@@ -128,4 +175,4 @@ if __name__ == "__main__":
         print(filename)
         img = sk.io.imread(os.path.join(
             input_folder_path, filename), as_gray=True)
-        mainPipeLine(img)
+        mainPipeLine(filename, img)
--- preproccessing/preprocessing.py
+++ preproccessing/preprocessing.py
@@ -20,7 +20,7 @@ def convertImgToUINT8(img_o):
 
 
 '''
-convert gray scale image to binary image 
+convert gray scale image to binary image
 '''
 
 
@@ -79,7 +79,7 @@ def runs_of_ones_array(bits):
 
 
 '''
-extract staff height and staff space based on run lenght encoding of white bits in binary representation of each column in the image 
+extract staff height and staff space based on run lenght encoding of white bits in binary representation of each column in the image
 ----based on what the papers was doing
 '''
 
@@ -142,7 +142,7 @@ def get_lines_rows(img, T_LEN):
 
 
 '''
-remove staff lines from binary image 
+remove staff lines from binary image
 '''
 
 
@@ -242,7 +242,7 @@ def restoreStaffLines(img, T_LEN, img_o):
 
 
 '''
-fix restored staff lines by connecting broken lines 
+fix restored staff lines by connecting broken lines
 '''
 
 
@@ -270,10 +270,14 @@ get charachters corners from the staff line
 
 
 def char_seg(org_img):
+    # show_images([org_img])
     img = np.copy(org_img)
-    img = img.astype(int)
+
+    toshow = [img]
+
     labels = sk.measure.label(img, connectivity=1)
     lbl_num = np.max(labels[:, :])
+
     bounds = np.zeros((lbl_num+1, 4))  # [up, down, left, right]
     bounds[:, 0] = 99999999
     bounds[:, 2] = 99999999
@@ -281,12 +285,22 @@ def char_seg(org_img):
     for i in range(img.shape[0]):
         for j in range(img.shape[1]):
             if img[i, j]:
-                bounds[labels[i, j]][0] = min(bounds[labels[i, j]][0], i)
-                bounds[labels[i, j]][1] = max(bounds[labels[i, j]][1], i)
-                bounds[labels[i, j]][2] = min(bounds[labels[i, j]][2], j)
-                bounds[labels[i, j]][3] = max(bounds[labels[i, j]][3], j)
+                bounds[labels[i, j]][0] = int(min(bounds[labels[i, j]][0], i))
+                bounds[labels[i, j]][1] = int(max(bounds[labels[i, j]][1], i))
+                bounds[labels[i, j]][2] = int(min(bounds[labels[i, j]][2], j))
+                bounds[labels[i, j]][3] = int(max(bounds[labels[i, j]][3], j))
 
-    return bounds
+    only_char_arr = []
+
+    for i in range(bounds.shape[0]):
+        if bounds[i][0] == 99999999:
+            continue
+        cur = np.copy(labels[int(bounds[i][0]):int(
+            bounds[i][1]+1), int(bounds[i][2]):int(bounds[i][3]+1)])
+        cur = cur == i
+        only_char_arr.append(cur)
+
+    return [bounds, only_char_arr]
 
 
 '''
